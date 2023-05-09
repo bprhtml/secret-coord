76281cd864176254f5731cbf3b1347165299711e
bprhtml <123954361+bprhtml@users.noreply.github.com>
2023-05-09 16:45:12 +0100

    Map reloads with each button clicked
    - Each button produces markers on a new map
    - New map array added (although only one location in so far)
    - Removed menu button at the top, will opt for a nav bar instead

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-// MENU
-
-const menuButton = document.querySelector('.menu-button')
-const dropdownMenu = document.querySelector('.dropdown-menu')
-const closeButton = document.querySelector('.close-button')
-menuButton.addEventListener('click', () => {
-  dropdownMenu.style.display = "flex"
-})
-closeButton.addEventListener('click', () => {
-  dropdownMenu.style.display = "none"
-})
-
-
-
 
 // MAP
 
 let markers = []
 const spanishSwim = document.getElementById('spanish-swim')
 const classicBeauty = document.getElementById('classic-beauty')
+const mainContainer = document.querySelector('.main-container')
+
+const devonBtn = document.createElement('button');
+devonBtn.innerHTML = "Devon Beauty Spots!";
+devonBtn.classList = "location-button";
+mainContainer.appendChild(devonBtn)
+
 const linkSwim = "https://www.wildswimming.co.uk/best-secret-beaches-coves-in-spain-for-wild-swimming/"
 //   [{ lat: , lng: }, ""],
-const secretLocations = [
+const spanishLocations = [
   [{ lat: 43.3631, lng: -8.3463}, "Praia Das Margaritas, Oleiros", `${linkSwim}`],
   [{ lat: 38.8175, lng: 0.1673 }, "La Cova Tallada, Costa Blanca"],
   [{ lat: 42.7922, lng: -9.0351}, "Praia Abelleira, Costa Galacia"],
@@ -31,20 +24,20 @@ const secretLocations = [
   [{ lat: 39.9268, lng: 3.9028 }, "Cala Des Talaier, Menorca"],
 ]
 
-const secretLocations2 = [  [{ lat: 37.9838, lng: 23.7275}, "Acropolis Museum, Athens"],
-  [{ lat: 48.8584, lng: 2.2945 }, "Eiffel Tower, Paris"],
-  [{ lat: 40.7851, lng: -73.9683}, "Central Park, New York"],
-  [{ lat: 51.5074, lng: -0.1278 }, "Big Ben, London"],
+const worldLocations = [  
+  [{ lat: 37.9838, lng: 23.7275}, "Acropolis Museum, Athens"],
+  [{ lat: 48.8584,  lng: 2.2945   }, "Eiffel Tower, Paris"],
+  [{ lat: 40.7851,  lng: -73.9683 }, "Central Park, New York"],
+  [{ lat: 51.5074,  lng: -0.1278  }, "Big Ben, London"],
   [{ lat: -22.9519, lng: -43.2105 }, "Christ the Redeemer, Rio de Janeiro"],
-  [{ lat: 55.7558, lng: 37.6173}, "Red Square, Moscow"],
-  [{ lat: -33.8599, lng: 151.2093}, "Sydney Opera House, Sydney"],
-  [{ lat: 37.7749, lng: -122.4194 }, "Golden Gate Bridge, San Francisco"],
+  [{ lat: 55.7558,  lng: 37.6173  }, "Red Square, Moscow"],
+  [{ lat: -33.8599, lng: 151.2093 }, "Sydney Opera House, Sydney"],
+  [{ lat: 37.7749,  lng: -122.4194}, "Golden Gate Bridge, San Francisco"],
 ]
 
-const map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 3.0,
-  center: { lat: 49.102, lng: 12.847 },
-});
+const devonLocations = [
+  [{ lat: 50.7063, lng: -4.0367}, "Meldon Reservoir"],
+]
 
 function initMapWithMarker(array) {
   printMarkers(array, map)
@@ -77,9 +70,25 @@ function printMarkers(array, map) {
 
 
 spanishSwim.addEventListener('click', () => {
-  printMarkers(secretLocations, map)
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5.5,
+    center: { lat: 40.4165, lng: -3.7026 },
+  });
+  printMarkers(spanishLocations, map)
 })
 
 classicBeauty.addEventListener('click', () => {
-  printMarkers(secretLocations2, map)
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 1.0,
+    center: { lat: 0, lng: 0 },
+  });
+  printMarkers(worldLocations, map)
+})
+
+devonBtn.addEventListener('click', () => {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8.0,
+    center: { lat: 50.734377, lng: -3.883316 },
+  });
+  printMarkers(devonLocations, map)
 })
